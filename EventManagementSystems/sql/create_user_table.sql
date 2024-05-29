068ee61dea38f6faf09610597405aec0b74a72e4
DayokmaStella <155432669+DayokmaStella@users.noreply.github.com>
2024-05-29 13:33:25 +0800

    Add files via upload

---
+++ EventManagementSystems/sql/create_user_table.sql
@@ -0,0 +1,12 @@
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `role` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
